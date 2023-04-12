a20946faf60ead07a9c17552e6f840ffeaa04e3b
Eeshan420 <89781115+Eeshan420@users.noreply.github.com>
2023-04-12 17:30:42 +0530

    Update streamlit_app.py
    added more code

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,11 @@ import streamlit
 
 streamlit.title('my parents new healty dinner')
 
-streamlit.header('Breakfast menu')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
 
-streamlit.text('omega 3 and blueberry oat meal')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 
